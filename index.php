5b1acae44606ef8e4a33409be84eb515157064a9
Stonuv <stonuv@gmail.com>
2025-11-08 20:20:02 +0300

    backup

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
 <?php
 // Путь к папке с миниатюрами
-$thumbDir = __DIR__ . '/thumbnails/';
+$img_dir = __DIR__ . '/full/';
+$thumbnail_dir = __DIR__ . '/thumbnails/';
 
-// Получаем список файлов
-$files = array_diff(scandir($thumbDir), ['.', '..']);
-
-// Фильтруем только файлы изображений (по расширению)
-$images = [];
-foreach ($files as $file) {
-    $path = $thumbDir . $file;
-    if (is_file($path)) {
-        // допустимые расширения
-        $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
-        if (in_array($ext, ['jpg','jpeg','png','gif','webp'])) {
-            $images[] = $file;
-        }
-    }
-}
+echo "Hello world!";
+echo "Good morning World!"
 ?>
